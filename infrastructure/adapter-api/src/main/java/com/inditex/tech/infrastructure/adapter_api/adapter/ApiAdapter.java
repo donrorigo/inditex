546fa4f0f8dacd2eca2c14eb15c2c5c113635200
Rodrigo Puerto Pedrera <ropuertop@gmail.com>
2023-07-05 00:06:38 +0200

    feature/version_1.0.0: applied google code style

--- infrastructure/adapter-api/src/main/java/com/inditex/tech/infrastructure/adapter_api/adapter/ApiAdapter.java
+++ infrastructure/adapter-api/src/main/java/com/inditex/tech/infrastructure/adapter_api/adapter/ApiAdapter.java
@@ -18,7 +18,10 @@ public class ApiAdapter implements ProductsApi {
 
   @Override
   public ResponseEntity<List<Integer>> _productsGet(final Boolean stock) {
-    final var products = Optional.ofNullable(stock).map(queryPresent -> this.findProductsStockFilterUseCase.execute()).orElse(this.findAllProductsUseCase.execute()).stream().map(product -> product.getId().intValue()).toList();
+    final var products = Optional.ofNullable(stock)
+        .map(queryPresent -> this.findProductsStockFilterUseCase.execute())
+        .orElse(this.findAllProductsUseCase.execute()).stream()
+        .map(product -> product.getId().intValue()).toList();
     return ResponseEntity.ok(products);
   }
 }
