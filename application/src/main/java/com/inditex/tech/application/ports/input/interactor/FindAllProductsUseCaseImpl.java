db001790b6e30840557c0b06b2cea482cc2f6edc
Rodrigo Puerto Pedrera <ropuertop@gmail.com>
2023-07-05 17:33:39 +0200

    feature/version_2.0.0: added transactional annotation to the application layer for readonly operations

--- application/src/main/java/com/inditex/tech/application/ports/input/interactor/FindAllProductsUseCaseImpl.java
+++ application/src/main/java/com/inditex/tech/application/ports/input/interactor/FindAllProductsUseCaseImpl.java
@@ -6,6 +6,7 @@ import com.inditex.tech.domain.entities.Product;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,7 @@ public class FindAllProductsUseCaseImpl implements FindAllProductsUseCase {
   private final ProductRepository repository;
 
   @Override
+  @Transactional(readOnly = true)
   public Collection<Product> execute() {
     return repository.findProducts();
   }
